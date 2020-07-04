e8c0d471c56bad733bcd5aeaf25c2cd16eca6efa
Thanh Lâm Nguyễn <lamnguyen11e@gmail.com>
2020-07-04 16:50:36 +0700

    complete responsive navbar

--- src/pages/homePage/components/navBar/navbar.js
+++ src/pages/homePage/components/navBar/navbar.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {RiSearchLine} from 'react-icons/ri'
+import {FiSettings} from 'react-icons/fi'
+import {GoSettings} from 'react-icons/go'
 import './navBar.scss'
 
 const Navbar = () => {
@@ -20,6 +22,11 @@ const Navbar = () => {
                 <img className="logo-algolia mr-1" src="https://logosandtypes.com/wp-content/uploads/algolia.svg" alt="logo"></img>
                 <span className="text-light text-algolia">Algolia</span>
             </div>
+            <a className="setting" href="/">
+                <FiSettings className="setting-icon"/>
+                <span className="text-setting">Settings</span>
+                <GoSettings className="setting-responsive-icon"/>
+            </a>
         </nav>
     )
 }
